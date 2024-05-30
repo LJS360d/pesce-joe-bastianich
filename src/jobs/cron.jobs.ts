6d6d02bf228d60bbdfe53ed89499ceed1c33fce7
LJS360d <luca.lencioni09@gmail.com>
2024-05-30 18:56:54 +0200

    add: better logging for create poll cron

--- src/jobs/cron.jobs.ts
+++ src/jobs/cron.jobs.ts
@@ -1,10 +1,14 @@
 import { CronJob } from 'cron';
 import { createPoll } from './create-poll';
+import { Logger } from 'fonzi2';
 
 async function startCrons() {
 	const thursday0800_createPoll = CronJob.from({
 		cronTime: '0 8 * * 4',
-		onTick: createPoll,
+		onTick: () => {
+			Logger.info('createPoll cron ticked');
+			createPoll();
+		},
 		start: true,
 		timeZone: 'Europe/Rome',
 	});
